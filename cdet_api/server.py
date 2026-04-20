a91f25d4bf26eb4477ad5cfb652b861dec602394
soboroff <ian.soboroff@nist.gov>
2026-04-20 14:12:14 -0400

    Added a finalize_run endpoint

--- cdet_api/server.py
+++ cdet_api/server.py
@@ -7,8 +7,8 @@ from annotated_types import doc
 from certifi import where
 from fastapi import FastAPI, Depends, HTTPException, Path, Query
 from fastapi.responses import StreamingResponse
-from pydantic import BaseModel, ConfigDict, StringConstraints
-from typing import List, Annotated
+from pydantic import BaseModel, ConfigDict, Field, RootModel, StringConstraints
+from typing import List, Annotated, Tuple
 from types import SimpleNamespace
 import tomllib
 import pathlib
@@ -86,7 +86,13 @@ class TopicResults(BaseModel):
     topic: str
     results: dict[DayString, List[QuestionResults]]
     extra: dict | None = None
-        
+
+class RunMetadata(BaseModel):
+    runtag: Annotated[str, Field(max_length=20)]
+    model_config = ConfigDict(extra='allow')
+
+Run = RootModel[Tuple[RunMetadata, List[TopicResults]]]
+
 # Dependency to safely manage database connections per request
 def get_db():
     try:
@@ -97,13 +103,13 @@ def get_db():
             db.close()
 
 @app.post('/start_run/{api_key}', dependencies=[Depends(get_db)])
-async def start_run(api_key: str, metadata: dict | None = None):
+async def start_run(api_key: str, runtag: str, metadata: dict | None = None):
     clean_run_states()  # Clean up old run states on each new run start
     if not valid_api_key(api_key):
         raise HTTPException(status_code=401, detail='Invalid API key')
     token = secrets.token_hex(23)
     # To do: store api_key, token pair
-    log(f'{token}.log', {'endpoint': '/start_run', 'api_key': api_key, 'metadata': metadata})
+    log(f'{token}.log', {'endpoint': '/start_run', 'api_key': api_key, 'runtag': runtag, 'metadata': metadata})
     RunState.insert(token=token, metadata={'state': 'started', 'api_key': api_key, 'timestamp': time.time()}).execute()
     return {'token': token}
 
@@ -161,16 +167,47 @@ async def retrieval(token: str,
         if docs:
             raise HTTPException(status_code=400, detail=f"Document {doc.docid} is from day {doc.day}, but the last accessed day for this run is {today}. Please ensure retrieval results are reported for the correct day.")
 
-    log(f'{token}.log', {'endpoint': '/retrieval', 'topic': topic, 'results': [ foo.model_dump() for foo in results ], 'metadata': metadata})
+    log(f'{token}.log', {'endpoint': '/retrieval', 'topic': topic, 'results': [ foo.model_dump() for foo in results ], 'retrieval_extra': metadata})
     return {'status': 'success'}
 
-@app.get('/finalize_run', dependencies=[Depends(get_db)])
+@app.get('/finalize_run', response_model=Run, dependencies=[Depends(get_db)])
 async def finalize_run(token: str):
     if not valid_token(token):
         raise HTTPException(status_code=401, detail='Invalid token')
     
-    log(f'{token}.log', {'endpoint': '/finalize_run'})
-    update_run_state(token, state='finalized')
+    results_per_topic = {}
+    errors = []
     with open(pathlib.Path(settings.logdir) / f'{token}.log', 'r') as f:
+        current_day = None
         for line in f:
             le = json.loads(line)
+
+            if le['endpoint'] == '/start_run':
+                yield {
+                    'runtag': le.get('runtag', 'my_run'),
+                    **le['metadata']
+                }
+
+            elif le['endpoint'] == '/next_day':
+                current_day = le['day']
+
+            elif le['endpoint'] == '/retrieval':
+                topic = le['topic']
+                if topic not in results_per_topic:
+                    results_per_topic[topic] = {
+                        'topic': topic,
+                        'extra': le['metadata'],
+                        'results': {}
+                    }
+                results_per_topic[topic]['results'][current_day] = le['results']
+
+    for topic in results_per_topic:
+        yield results_per_topic[topic]
+
+    if len(errors) > 0:
+        yield { 'errors': errors }
+
+    log(f'{token}.log', {'endpoint': '/finalize_run'})
+    update_run_state(token, state='finalized')
+    RunState.delete().where(RunState.token == token).execute()
+
